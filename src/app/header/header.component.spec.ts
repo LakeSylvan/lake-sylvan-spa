73599403261683cf2f18e5125b38beb8328bbd97
Lake Sylvan <lakesylvanoh@gmail.com>
2023-04-15 17:35:23 -0400

    FEIGEB-baseline_app

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -19,7 +19,42 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  describe('getHeaderImageForSeason', () => {
+    it('should return the spring image url if the date is March 20th', () => {
+      const utl = 'assets/images/sylvan-spring-pano.jpg';
+      expect(component.getHeaderImageForSeason(new Date(2023, 2, 20))).toEqual(utl)
+    });
+    it('should return the spring image url if the date is June 20th', () => {
+      const utl = 'assets/images/sylvan-spring-pano.jpg';
+      expect(component.getHeaderImageForSeason(new Date(2023, 5, 20))).toEqual(utl)
+    });
+    it('should return the summer image url if the date is June 21th', () => {
+      const utl = 'assets/images/sylvan-pano.jpg';
+      expect(component.getHeaderImageForSeason(new Date(2023, 6, 21))).toEqual(utl)
+    });
+    it('should return the summer image url if the date is September 22nd', () => {
+      const utl = 'assets/images/sylvan-pano.jpg';
+      expect(component.getHeaderImageForSeason(new Date(2023, 8, 22))).toEqual(utl)
+    });
+    it('should return the fall image url if the date is September 23nd', () => {
+      const utl = 'assets/images/sylvan-fall-pano.jpg';
+      expect(component.getHeaderImageForSeason(new Date(2023, 8, 23))).toEqual(utl)
+    });
+    it('should return the fall image url if the date is December 21st', () => {
+      const utl = 'assets/images/sylvan-winter-pano.jpg';
+      expect(component.getHeaderImageForSeason(new Date(2023, 11, 21))).toEqual(utl)
+    });
+    it('should return the first winter image url if the date is December 22st', () => {
+      const utl = 'assets/images/sylvan-winter-pano.jpg';
+      expect(component.getHeaderImageForSeason(new Date(2023, 11, 21))).toEqual(utl)
+    });
+    it('should return the first winter image url if the date is March 19th', () => {
+      const utl = 'assets/images/sylvan-winter-pano.jpg';
+      expect(component.getHeaderImageForSeason(new Date(2023, 2, 19))).toEqual(utl)
+    });
+    it('should return the second winter image url if the date is in January', () => {
+      const utl = 'assets/images/sylvan-winter2-pano.jpg';
+      expect(component.getHeaderImageForSeason(new Date(2023, 0, 19))).toEqual(utl)
+    });
+  })
 });
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import AWS from 'aws-sdk'
+// import * as AWS from 'aws-sdk';
+// var AWS = require('aws-sdk');
+AWS.config.update({
+  region: "us-east-2",
+  accessKeyId: "AKIAJBVGC73WU23JUOZA",
+  secretAccessKey: "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"
+});
 
 @Component({
   selector: 'header',
@@ -7,9 +15,95 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  dynamodb = new AWS.DynamoDB();
+  header: any;
+  headerImage: string | undefined;
+  message: string | undefined;
+  isWarning: boolean | undefined;
 
-  ngOnInit(): void {
+  constructor() {
+    this.header = {
+      url: 'assets/images/header.JPG',
+      display: true,
+    };
+    this.headerImage = this.getHeaderImageForSeason(new Date());
+  }
+
+  ngOnInit() {
+    this.getMessage();
+  }
+
+  getHeaderImageForSeason(now: Date): string {
+    var currentYear = now.getUTCFullYear();
+    let currentDate = new Date(currentYear, now.getUTCMonth(), now.getUTCDate())
+    let springStartDate = new Date(currentYear, 2, 20)
+    let summerStartDate = new Date(currentYear, 5, 21)
+    let fallStartDate = new Date(currentYear, 8, 23)
+    let winterStartDate = new Date(currentYear, 11, 21)
+
+    let imagePath = 'assets/images/sylvan-winter-pano.jpg'
+    if ( currentDate >= springStartDate && currentDate < summerStartDate ) {
+      imagePath = 'assets/images/sylvan-spring-pano.jpg';
+    } else if (currentDate >= summerStartDate && currentDate < fallStartDate) {
+      imagePath = 'assets/images/sylvan-pano.jpg';
+    } else if (currentDate >= fallStartDate && currentDate < winterStartDate) {
+      imagePath = 'assets/images/sylvan-fall-pano.jpg';
+    } else if (now.getUTCMonth() ==0) {
+      imagePath = 'assets/images/sylvan-winter2-pano.jpg'
+    }
+    return imagePath;
+  }
+
+  saveMessage(message: string, isWarning: boolean): void{
+    var updateParams = {
+      "TableName": "LS.Message",
+      "Key": {
+          "MessageId": { "S": "001"}
+      },
+      "UpdateExpression": "SET #T = :t, #W = :w",
+      "ExpressionAttributeNames": {
+          "#T": "Text",
+          "#W": "WarningInd"
+      },
+      "ExpressionAttributeValues": {
+          ":t": { "S": message},
+          ":w": { "BOOL": isWarning}
+      }
+    };
+    this.dynamodb.updateItem(updateParams).promise()
+    .then((results) => {
+      //Return a message to the user???
+    })
+    .catch();
+  }
+
+  getMessage(): void {
+    var getItemParams = {
+      "TableName": "LS.Message",
+      "Key": {
+          "MessageId": {
+              "S": "001"
+          }
+      },
+    };
+    this.dynamodb.getItem(getItemParams).promise()
+    .then((results: any) => {
+      this.setMessage(results.Item.Text.S);
+      this.setIsWarning(results.Item.WarningInd.BOOL)
+    })
+    .catch();
+  }
+
+  setMessage(inMessage: string): void{
+    this.message = inMessage;
+  }
+
+  setIsWarning(isWarningInd: boolean): void {
+    this.isWarning = isWarningInd
+  }
+
+  dismissWarning(): void{
+    this.message = undefined;
   }
 
 }
